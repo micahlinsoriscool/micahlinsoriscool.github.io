d2575a02c0f8deab2691534ad10f441b5dae8c15
micahlinsoriscool <linser.micah@stu.jamesirwin.org>
2024-10-16 08:53:27 -0600

    fsdhivjhifsdjfsduifsduifsdui

---
+++ main.py
@@ -0,0 +1,17 @@
+print("hi")
+print("my name is micah")
+print("ooo")
+print(" o")
+my_string="hi"
+my_float=3.53224532
+my_int=42
+my_bool=True
+my_none=None
+
+my_list=[1,2,3,4,5]
+my_set={1,2,3,4,5}
+
+
+dict
+
+def
